77b3e35746d7885b832d29033ccbef6d9a6b1266
kkilledar1 <33131185+kkilledar1@users.noreply.github.com>
2017-10-30 11:28:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def makeWebhookResult(req):
         "speech": speech,
         "displayText": speech,
         #"data": {},
-        "contextOut": [],
+        "contextOut": ["CIRstat":CIRstatus_res],
         "source": "apiai-CIR-status"
     }
     data = result.json()
